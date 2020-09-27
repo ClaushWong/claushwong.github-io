8ec21cc8e883fce676a7da85eca8c458f4b8f6aa
ClaushWong <claushwong123@gmail.com>
2020-09-28 01:03:09 +0800

    Add more questions

--- gamedata.js
+++ gamedata.js
@@ -24,7 +24,7 @@ data = [
         "correct": "c"
     },
     {
-        "question": "Which Operating Software ( OS ) doesn't exist?",
+        "question": "Which Operating Software(OS) doesn't exist?",
         "a": "Knopiz",
         "b": "Kubuntu",
         "c": "Window",
@@ -38,5 +38,32 @@ data = [
         "c": ".ava",
         "d": ".gif",
         "correct": "d"
+    },
+    {
+        "question": "<img src='https://vignette.wikia.nocookie.net/virtualyoutuber/images/0/05/Inugami_Korone_img.png/revision/latest/top-crop/width/720/height/900?cb=20190405185913' width='150' height='200'>"+
+                    "<br><br>Who is this?",
+        "a": "Inuyama Tamaki",
+        "b": "Inugami Korone",
+        "c": "Tokoyami Towa",
+        "d": "Nekomata Okayu",
+        "correct": "b"
+    },
+    {
+        "question": "<img src='https://edm.com/.image/t_share/MTc0NjYyNjI0ODg4MjM1Mzg2/science-of-earworms-explain-why-never-gonna-give-you-up-is-stuck-in-our-heads-30-years-later.png' width='200' height='200'"+
+                    "<br><br>Who is this?",
+        "a": "Rick Ross",
+        "b": "Rick Martin",
+        "c": "Rick Springfield",
+        "d": "Rick Astley",
+        "correct" :"d"
+    },
+    {
+        "question": "<iframe src='https://www.youtube.com/embed/O5STxNb0vkQ' width='400' height='200'>"+
+                    "<br><br>In this video, how many HA did Pekora laugh?",
+        "a": "7",
+        "b": "8",
+        "c": "9",
+        "d": "TONS",
+        "correct": "c"
     }
 ]
