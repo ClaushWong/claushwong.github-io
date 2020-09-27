65cc8b30ba13372f5b16ac28e7bb25284eb24258
ClaushWong <claushwong123@gmail.com>
2020-09-28 00:25:42 +0800

    Add QuizGame+

---
+++ gamedata.js
@@ -0,0 +1,42 @@
+data = [
+    {
+        "question": "Who is the founder of computer?",
+        "a": "Augusta Ada Lovelace",
+        "b": "Charles Babbage",
+        "c": "Bill Gates",
+        "d": "Steve Jobs",
+        "correct": "b"
+    },
+    {
+        "question": "Which of the following flower doesn't exist?",
+        "a": "Poffy",
+        "b": "Rose",
+        "c": "Ren",
+        "d": "Bluebell",
+        "correct": "a"
+    },
+    {
+        "question": "Which company in below doesn't produce processors?",
+        "a": "IBM",
+        "b": "Intel",
+        "c": "Apple Co.",
+        "d": "Marvell",
+        "correct": "c"
+    },
+    {
+        "question": "Which Operating Software ( OS ) doesn't exist?",
+        "a": "Knopiz",
+        "b": "Kubuntu",
+        "c": "Window",
+        "d": "UNIX",
+        "correct": "a"
+    },
+    {
+        "question": "Which is the real file format for video.",
+        "a": ".mp3",
+        "b": ".wmf",
+        "c": ".ava",
+        "d": ".gif",
+        "correct": "d"
+    }
+]
