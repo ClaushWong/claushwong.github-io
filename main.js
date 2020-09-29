382c61f81a395f74405e798e050a3f8adff7cb82
ClaushWong <claushwong123@gmail.com>
2020-09-29 09:13:04 +0800

    Add time and favicon

--- main.js
+++ main.js
@@ -186,6 +186,24 @@ function displayTable()
     addBody(table)
 }
 
+function startTime()
+{
+    let today = new Date()
+    let h = today.getHours()
+    let m = today.getMinutes()
+    let s = today.getSeconds()
+    h = checkTime(h)
+    m = checkTime(m)
+    s = checkTime(s)
+    document.getElementById('thyme').innerHTML = h + ":" + m + ":" + s
+    let t = setTimeout(startTime, 500)
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
+    return i
+}
+
 // Fetch API
 fetch('https://raw.githubusercontent.com/ClaushWong/notes/master/schedule.json')
     .then((res) => res.json())
